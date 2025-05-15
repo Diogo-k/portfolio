6d50d77393040300544c49a921a375c2dd8bdd46
Diogo-k <jdiogok@gmail.com>
2025-05-16 00:33:31 +0100

    Update next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,17 @@
 /** @type {import('next').NextConfig} */
-
-import withBundleAnalyzer from '@next/bundle-analyzer';
+// import withBundleAnalyzer from '@next/bundle-analyzer';
 
 const nextConfig = {
+    serverExternalPackages: ['three', 'react-three/fiber', 'react-three/drei'],
+    experimental: {
+        optimizePackageImports: ['@icons-pack/react-simple-icons'],
+    },
     eslint: {
         ignoreDuringBuilds: true,
     },
 };
 
-export default withBundleAnalyzer({
-    enabled: process.env.ANALYZE === 'true',
-})(nextConfig);
+// export default withBundleAnalyzer({
+//     enabled: process.env.ANALYZE === 'true',
+// })(nextConfig);
+export default nextConfig;
